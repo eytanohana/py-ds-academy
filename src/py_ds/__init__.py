e3d949b1231a80a0e9f7b1aa9d1e57bd79344fee
eytanohana <eohana98@gmail.com>
2025-12-01 20:10:27 +0200

    try new build backend for versioning

--- src/py_ds/__init__.py
+++ src/py_ds/__init__.py
@@ -2,9 +2,19 @@ from py_ds.datastructures.linked_list import DoublyLinkedList, SinglyLinkedList
 from py_ds.datastructures.queue import Queue
 from py_ds.datastructures.stack import Stack
 
+import importlib.metadata
+
 __all__ = [
     "DoublyLinkedList",
     "Queue",
     "SinglyLinkedList",
     "Stack",
 ]
+
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+print(__version__)

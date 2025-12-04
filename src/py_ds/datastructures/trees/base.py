13ceada3c04239729c29cf93fc4cb10182b2163f
eytanohana <eohana98@gmail.com>
2025-12-04 22:50:40 +0200

    start binary tree

---
+++ src/py_ds/datastructures/trees/base.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from collections.abc import Iterator
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+@dataclass
+class _Node(Generic[T]):
+    """A node with references to its left and right child nodes."""
+
+    value: T
+    left: _Node[T] | None = None
+    right: _Node[T] | None = None
+
+
+class BinaryTree(ABC, Generic[T]):
+    @abstractmethod
+    def insert(self, value: T) -> None:
+        """Add a value to the end of the tree."""
+
+    @abstractmethod
+    def remove(self, value: T) -> None:
+        """Remove the first occurrence of a value."""
+
+    @abstractmethod
+    def clear(self) -> None:
+        """Remove all elements."""
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[T]:
+        """Iterate through values."""

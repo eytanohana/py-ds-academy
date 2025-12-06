f06be5f5dd2fa26f690a8ec01bed05d08ef6b1f9
eytanohana <eohana98@gmail.com>
2025-12-06 22:50:34 +0200

    implement basic

--- src/py_ds/datastructures/heaps/MinHeap.py
+++ src/py_ds/datastructures/heaps/MinHeap.py
@@ -5,9 +5,20 @@ T = TypeVar("T")
 
 
 class MinHeap(Generic[T]):
-    def __init__(self, items: Iterable[T] | None = None): ...
+    def __init__(self, items: Iterable[T] | None = None):
+        items = items or []
+        self._size = 0
+        for item in items:
+            self.push(item)
+
     def push(self, item: T) -> None: ...
+
     def pop(self) -> T: ...
+
     def peek(self) -> T: ...
-    def __len__(self) -> int: ...
-    def __bool__(self) -> bool: ...
+
+    def __len__(self) -> int:
+        return self._size
+
+    def __bool__(self) -> bool:
+        return self._size > 0

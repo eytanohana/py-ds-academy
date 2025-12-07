4e5a26c6f57cd742834c62d086214faf5154fead
eytanohana <eohana98@gmail.com>
2025-12-07 23:28:20 +0200

    insert

--- src/py_ds/datastructures/trees/avl.py
+++ src/py_ds/datastructures/trees/avl.py
@@ -3,5 +3,20 @@ from py_ds.datastructures.trees.binary_search_tree import BinarySearchTree
 
 
 class AVLTree(BinarySearchTree[T]):
-    def _balance_factor(self, node: _BinaryNode[T]):
+    def _balance_factor(self, node: _BinaryNode[T]) -> int:
         return self._height(node.left) - self._height(node.right)
+
+    def _rebalance(self, node: _BinaryNode[T]) -> None: ...
+
+    def _insert_recursive(self, node: _BinaryNode[T] | None, value: T) -> _BinaryNode[T]:
+        if node is None:
+            return _BinaryNode(value=value)
+        if value <= node.value:
+            node.left = self._insert_recursive(node.left, value)
+        else:
+            node.right = self._insert_recursive(node.right, value)
+        self._rebalance(node)
+
+    def insert(self, value: T) -> None:
+        self._root = self._insert_recursive(self._root, value)
+        self.size += 1

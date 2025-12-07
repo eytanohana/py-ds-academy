51054210b2ba4a95704636d40db0a71a1e2802aa
eytanohana <eohana98@gmail.com>
2025-12-08 01:13:38 +0200

    implement rotations

--- src/py_ds/datastructures/trees/avl.py
+++ src/py_ds/datastructures/trees/avl.py
@@ -3,10 +3,48 @@ from py_ds.datastructures.trees.binary_search_tree import BinarySearchTree
 
 
 class AVLTree(BinarySearchTree[T]):
+    def _height(self, node: _BinaryNode[T] | None) -> int:
+        if node is None:
+            return -1
+        return 1 + max(self._height(node.left), self._height(node.right))
+
     def _balance_factor(self, node: _BinaryNode[T]) -> int:
         return self._height(node.left) - self._height(node.right)
 
-    def _rebalance(self, node: _BinaryNode[T]) -> None: ...
+    @staticmethod
+    def _rotate_right(node: _BinaryNode[T]) -> _BinaryNode[T]:
+        new_root = node.left
+        node.left = new_root.right
+        new_root.right = node
+        return new_root
+
+    @staticmethod
+    def _rotate_left(node: _BinaryNode[T]) -> _BinaryNode[T]:
+        new_root = node.right
+        node.right = new_root.left
+        new_root.left = node
+        return new_root
+
+    def _rotate_left_right(self, node: _BinaryNode[T]) -> None:
+        node.left = self._rotate_left(node.left)
+        return self._rotate_right(node)
+
+    def _rotate_right_left(self, node: _BinaryNode[T]) -> None:
+        node.right = self._rotate_right(node.right)
+        return self._rotate_left(node)
+
+    def _rebalance(self, node: _BinaryNode[T]) -> _BinaryNode[T]:
+        bf = self._balance_factor(node)
+        if bf > 1:
+            if self._balance_factor(node.left) > 0:
+                return self._rotate_right(node)
+            return self._rotate_left_right(node)
+
+        if bf < -1:
+            if self._balance_factor(node.right) < 0:
+                return self._rotate_left(node)
+            return self._rotate_right_left(node)
+        return node
 
     def _insert_recursive(self, node: _BinaryNode[T] | None, value: T) -> _BinaryNode[T]:
         if node is None:
@@ -15,7 +53,7 @@ class AVLTree(BinarySearchTree[T]):
             node.left = self._insert_recursive(node.left, value)
         else:
             node.right = self._insert_recursive(node.right, value)
-        self._rebalance(node)
+        return self._rebalance(node)
 
     def insert(self, value: T) -> None:
         self._root = self._insert_recursive(self._root, value)

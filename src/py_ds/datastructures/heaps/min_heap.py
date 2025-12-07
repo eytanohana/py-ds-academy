a7a6ada10fee5f3880fc785a98567199e4f6cf90
eytanohana <eohana98@gmail.com>
2025-12-07 11:40:43 +0200

    add base class

--- src/py_ds/datastructures/heaps/min_heap.py
+++ src/py_ds/datastructures/heaps/min_heap.py
@@ -1,55 +1,15 @@
-from collections.abc import Iterable
-from typing import Generic, TypeVar
-
-T = TypeVar('T')
+from py_ds.datastructures.heaps.base import Heap
 
 
-class MinHeap(Generic[T]):
-    def __init__(self, items: Iterable[T] | None = None):
-        items = items or []
-        self._items: list[T] = []
-        self._size: int = 0
-        for item in items:
-            self.push(item)
-
-    def _swap(self, idx1, idx2) -> None:
-        self._items[idx1], self._items[idx2] = self._items[idx2], self._items[idx1]
-
-    @staticmethod
-    def _left_index(index) -> int:
-        return 2 * index + 1
-
-    @staticmethod
-    def _right_index(index) -> int:
-        return 2 * index + 2
-
-    def _has_left_child(self, index) -> bool:
-        return self._left_index(index) < self._size
-
-    def _has_right_child(self, index) -> bool:
-        return self._right_index(index) < self._size
-
-    def _left_child(self, index) -> T:
-        return self._items[self._left_index(index)]
-
-    def _right_child(self, index) -> T:
-        return self._items[self._right_index(index)]
-
-    def _heapify_up(self, index: int) -> None:
+class MinHeap(Heap):
+    def _heapify_up(self) -> None:
+        index = self._size - 1
         while index > 0 and self._items[index] < self._items[parent_idx := (index - 1) // 2]:
             self._swap(index, parent_idx)
             index = parent_idx
 
-    def push(self, item: T) -> None:
-        index = self._size
-        if index >= len(self._items):
-            self._items.append(item)
-        else:
-            self._items[index] = item
-        self._heapify_up(index)
-        self._size += 1
-
-    def _heapify_down(self, parent_idx: int) -> None:
+    def _heapify_down(self) -> None:
+        parent_idx = 0
         while self._has_left_child(parent_idx):
             smaller_child, smaller_child_idx = self._left_child(parent_idx), self._left_index(parent_idx)
             if self._has_right_child(parent_idx) and (right_child := self._right_child(parent_idx)) < smaller_child:
@@ -60,24 +20,3 @@ class MinHeap(Generic[T]):
                 parent_idx = smaller_child_idx
             else:
                 break
-
-    def pop(self) -> T:
-        if not self:
-            raise IndexError('pop from an empty heap')
-        parent_idx = 0
-        item = self._items[parent_idx]
-        self._items[parent_idx] = self._items[self._size - 1]
-        self._size -= 1
-        self._heapify_down(parent_idx)
-        return item
-
-    def peek(self) -> T:
-        if not self:
-            raise IndexError('peek from an empty heap')
-        return self._items[0]
-
-    def __len__(self) -> int:
-        return self._size
-
-    def __bool__(self) -> bool:
-        return self._size > 0

a5d6a39ef5ed670026acaf34cd70d0a85c263422
eytanohana <eohana98@gmail.com>
2025-12-27 10:37:54 +0200

    update tests

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,10 +1,10 @@
 import pytest
 
-from py_ds.datastructures.linked_lists.singly_linked import SinglyLinkedList
+from py_ds.datastructures.linked_lists.singly_linked import LinkedList
 
 
 def test_empty_list_initial_state():
-    ll: SinglyLinkedList[int] = SinglyLinkedList()
+    ll: LinkedList[int] = LinkedList()
     assert len(ll) == 0
     assert list(ll) == []
     assert ll.head() is None
@@ -13,7 +13,7 @@ def test_empty_list_initial_state():
 
 
 def test_init_with_items():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     assert len(ll) == 3
     ll_list = list(ll)
     assert ll_list == [1, 2, 3]
@@ -22,7 +22,7 @@ def test_init_with_items():
 
 
 def test_append_adds_to_end():
-    ll = SinglyLinkedList[int]()
+    ll = LinkedList[int]()
     ll.append(1)
     ll.append(2)
     ll.append(3)
@@ -33,7 +33,7 @@ def test_append_adds_to_end():
 
 
 def test_prepend_adds_to_front():
-    ll = SinglyLinkedList[int]()
+    ll = LinkedList[int]()
     ll.prepend(3)
     assert ll.head() == 3 and ll.tail() == 3
     ll.prepend(2)
@@ -44,7 +44,7 @@ def test_prepend_adds_to_front():
 
 
 def test_positive_insert_at_beginning_middle_and_end():
-    ll = SinglyLinkedList([1, 3, 4])
+    ll = LinkedList([1, 3, 4])
 
     # insert at beginning
     ll.insert(0, 0)
@@ -61,7 +61,7 @@ def test_positive_insert_at_beginning_middle_and_end():
 
 
 def test_negative_insert_at_beginning_middle_and_end():
-    ll = SinglyLinkedList([1, 3, 4])
+    ll = LinkedList([1, 3, 4])
 
     # insert at beginning
     ll.insert(-1, 0)
@@ -82,7 +82,7 @@ def test_negative_insert_at_beginning_middle_and_end():
 
 
 def test_insert_out_of_bounds_raises():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     with pytest.raises(IndexError):
         ll.insert(10, 99)
     with pytest.raises(IndexError):
@@ -90,7 +90,7 @@ def test_insert_out_of_bounds_raises():
 
 
 def test_remove_existing_value():
-    ll = SinglyLinkedList([1, 2, 3, 2])
+    ll = LinkedList([1, 2, 3, 2])
 
     ll.remove(2)  # remove first occurrence
     assert list(ll) == [1, 3, 2]
@@ -108,20 +108,20 @@ def test_remove_existing_value():
 
 
 def test_remove_nonexistent_raises():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     with pytest.raises(ValueError):
         ll.remove(99)
 
 
 def test_pop_default_pops_last():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     value = ll.pop()
     assert value == 3
     assert ll.tail() == 2
 
 
 def test_pop_positive_index():
-    ll = SinglyLinkedList([1, 2, 3, 4, 5])
+    ll = LinkedList([1, 2, 3, 4, 5])
     value = ll.pop(0)
     assert value == 1
     assert list(ll) == [2, 3, 4, 5]
@@ -137,7 +137,7 @@ def test_pop_positive_index():
 
 
 def test_pop_negative_index():
-    ll = SinglyLinkedList([1, 2, 3, 4, 5])
+    ll = LinkedList([1, 2, 3, 4, 5])
     value = ll.pop(-2)
     assert value == 4
     assert list(ll) == [1, 2, 3, 5]
@@ -152,17 +152,17 @@ def test_pop_negative_index():
 
 
 def test_str_and_repr():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     assert str(ll) == 'HEAD → 1 → 2 → 3 → NULL'
-    assert repr(ll) == 'SinglyLinkedList([1, 2, 3])'
+    assert repr(ll) == 'LinkedList([1, 2, 3])'
 
-    ll2 = SinglyLinkedList()
+    ll2 = LinkedList()
     assert str(ll2) == 'HEAD → NULL'
-    assert repr(ll2) == 'SinglyLinkedList([])'
+    assert repr(ll2) == 'LinkedList([])'
 
 
 def test_get_item():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     assert ll[0] == 1
     assert ll[1] == 2
     assert ll[2] == 3
@@ -177,7 +177,7 @@ def test_get_item():
 
 
 def test_set_item():
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
     ll[0] = 10
     assert ll[0] == 10
 
@@ -190,7 +190,7 @@ def test_set_item():
     with pytest.raises(IndexError):
         ll[3] = 40
 
-    ll = SinglyLinkedList([1, 2, 3])
+    ll = LinkedList([1, 2, 3])
 
     ll[-1] = 30
     assert ll[-1] == 30
